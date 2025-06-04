937ed7938051a8e9d143b51dd1cc7a24a0259451
sadia123parveen <bsf2104281@ue.edu.pk>
2025-06-04 19:16:04 +0500

    Task Management app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,29 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  template:`
+  <div class='container'>
+  <h2>My Todays Challanges</h2>
+  </div>
+  <div class='task-card'>
+  @for(task of Tasks; track task.id){
+   <h3 [style.color]="task.completed === 'True' ? 'green' : 'red' ">{{task.name}}  
+   <button (click)="onClick(task)">Complete</button></h3><br><br>
+  
+        @if (task.completed === 'True') {
+          <p [style.color]="'green'">✅ Task Completed</p>
+        }
+   }</div>
+   `,
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'First-Angular-Tutorial';
+  Tasks = [{id:1, name:'Morning walk', completed:'False'}
+    ,{id:2, name:'Take Break fast',  completed:'False'},
+    {id:3, name:'Morning walk'},
+    {id:4, name:'Clean your home'},
+    {id:5, name:'Go to work desk',  completed:'False'}];
+    onClick(task:any){
+      task.completed = 'True'
+    }
 }
